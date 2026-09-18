ffdbaca97a99718afcc782c7240fdb387c71297e
Amanmishra30 <amanmishra6962@gmail.com>
2026-09-18 23:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (29.61%) - LeetSync

---
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+        int cnt[] = new int [101];
+        for(int i :nums) cnt[i]++;
+        int pairs =0;
+        int leftover = 0;
+        for(int i :cnt){
+            pairs += i/2;
+            leftover += i%2;
+        }
+   return new int []{pairs, leftover};
+    }
+}
